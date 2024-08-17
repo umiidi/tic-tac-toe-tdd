5dc673ac5f973dbd02f18842cac8e5ee1bb5e0ed
Umid <vvumid@gmail.com>
2024-08-17 08:52:14 +0400

    tdd with mock

---
+++ src/main/java/az/ingress/TicTacToeRepo.java
@@ -0,0 +1,28 @@
+package az.ingress;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class TicTacToeRepo {
+
+    private final List<TicTacToe> boardState = new ArrayList<>();
+
+    void saveMove(TicTacToe ticTacToe) {
+        ticTacToe.setId(boardState.size());
+        boardState.add(ticTacToe);
+    }
+
+    List<TicTacToe> getBoardState() {
+        System.out.println("esl repo");
+        return new LinkedList<>(boardState); // immutable
+    }
+
+    TicTacToe getLastMove() {
+        if (boardState.isEmpty()) {
+            return null;
+        }
+        return boardState.get(boardState.size() - 1);
+    }
+
+}
